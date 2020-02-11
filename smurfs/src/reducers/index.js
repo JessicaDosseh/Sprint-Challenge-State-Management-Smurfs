d4387e17808b4264273c3c31aa793b9f2180c143
Jessica <Jessica@users-iMac.local>
2020-02-10 20:01:26 -0500

    MVP

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,64 @@
+import{
+  FETCH_DATA_START,
+  FETCH_DATA_SUCCESS,
+  FETCH_DATA_FAILURE,
+  POST_DATA_START,
+  POST_DATA_SUCCESS,
+  POST_DATA_FAILURE,
+} from "../actions/index";
+
+
+const initialState = {
+  smurf:[],
+  error: "",
+  isFetching: false
+}
+
+const smurfReducer = (state = initialState, action)=>{
+  switch(action.type){
+
+      case FETCH_DATA_START:
+          return{
+              ...state, 
+              error: "", 
+              isFetching:true
+          };
+      case FETCH_DATA_SUCCESS:
+          return{
+              ...state, 
+              error: "", 
+              isFetching: false, 
+              smurf: action.payload
+          };
+      case FETCH_DATA_FAILURE:
+          return{
+              ...state, 
+              error: action.payload, 
+              isFetching:false
+          };
+ // ------------------------------------------- // 
+      case POST_DATA_START:
+          return{ 
+              ...state, 
+              error: "",
+              isFetching:true
+          };
+      case POST_DATA_SUCCESS:
+          return{
+              ...state, 
+              error: "", 
+              isFetching:false, 
+              smurf: action.payload
+          };
+      case POST_DATA_FAILURE:
+          return{
+              ...state, 
+              error: action.payload,
+              isFetching: false
+          };
+      default:
+          return state;
+  }
+}
+
+export default smurfReducer; 
